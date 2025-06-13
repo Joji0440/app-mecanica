a3bb8dfa516eb4c7568ce431f3bcf2f195054848
Lizzardi Anthoni Milazzo Cedeño <e1316892304@live.uleam.edu.ec>
2025-06-12 19:09:28 -0500

    .

--- Mecanica/app/Http/Kernel.php
+++ Mecanica/app/Http/Kernel.php
@@ -6,14 +6,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class, // Protección CSRF
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
